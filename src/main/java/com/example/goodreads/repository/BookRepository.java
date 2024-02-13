94338e99d9d1fb0ff17c88427abb49cc5515fba1
syam-sundar-pendreddi <syamsundar302@gmail.com>
2024-02-13 06:36:28 +0000

    solution

---
+++ src/main/java/com/example/goodreads/repository/BookRepository.java
@@ -0,0 +1,17 @@
+package com.example.goodreads.repository;
+
+import java.util.ArrayList;
+import com.example.goodreads.model.Book;
+
+public interface BookRepository {
+    ArrayList<Book> getBooks();
+
+    Book getBookById(int bookId);
+
+    Book addBook(Book book);
+
+    Book updateBook(int bookId, Book book);
+
+    void deleteBook(int bookId);
+
+}
